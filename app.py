fe554601068a472ac0d51bd9a7cb331d0f46cd75
himanshupandey04 <150778405+himanshupandey04@users.noreply.github.com>
2025-07-25 15:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,36 +3,36 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 def rule_based_discount(category, brand, season, price, stock, rating):
-    discount = 10  # base discount
-    # Category adjustment
+    discount = 10  
+    
     if category == 'Jacket':
         discount += 10
     elif category == 'T-Shirt':
         discount += 5
-    # Brand adjustment
+    
     if brand == 'Adidas':
         discount -= 2
     elif brand == 'Nike':
         discount -= 1
-    # Season adjustment
+    
     if season == 'Festive':
         discount += 8
     elif season == 'Winter':
         discount += 3
-    # Price adjustment
+    
     if price > 50000:
         discount += 7
     elif price > 20000:
         discount += 3
-    # Stock adjustment
+    
     if stock > 300:
         discount += 4
-    # Rating adjustment
+    
     if rating > 4.5:
         discount -= 3
     elif rating < 2:
         discount += 2
-    # Clamp discount between 5 and 80
+    
     return max(5, min(discount, 80))
 
 @app.route('/predict', methods=['POST'])
